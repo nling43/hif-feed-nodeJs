89837d40e9eb706494e0a169eb17ff5f2d7aab12
Niklas Ling <72392907+nling43@users.noreply.github.com>
2022-12-07 15:46:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const moment = require("moment");
-const { json } = require("express");
+
 const app = express();
+app.use(express.json());
+app.use(require("body-parser").json());
 const PORT = process.env.PORT || 3001;
 
 async function fs() {
